6108bc80b485c70379def5548411b47b3b124c21
BoyanK95 <boyan_k_95@abv.bg>
2026-04-07 01:53:32 +0300

    fix: error mismatch with next auth version

--- src/server/auth/index.ts
+++ src/server/auth/index.ts
@@ -1,10 +1,10 @@
 import NextAuth from "next-auth";
-import { getServerSession } from "next-auth";
+import { cache } from "react";
 
 import { authConfig } from "./config";
 
-const { handlers, signIn, signOut } = NextAuth(authConfig);
+const { auth: uncachedAuth, handlers, signIn, signOut } = NextAuth(authConfig);
 
-const auth = () => getServerSession(authConfig);
+const auth = cache(uncachedAuth);
 
 export { auth, handlers, signIn, signOut };
