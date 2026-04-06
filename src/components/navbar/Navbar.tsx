bee5672108c9c75f0bcd2b111db2c71228a6899a
BoyanK95 <boyan_k_95@abv.bg>
2026-04-07 00:11:02 +0300

    feat: create session provider

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -8,8 +8,18 @@ import UserDropdown from "./UserDropdown";
 import ShopingCartButton from "./ShopingCartButton";
 import HamburgerMenu from "./HamburgerMenu";
 import { ModeToggle } from "../theme-provider/theme-mode-toggle";
+import { useSession } from "next-auth/react";
+import { Button } from "../ui/button";
+import Link from "next/link";
+import { ROUTES } from "@/lib/constants/routes";
 
 const Navbar = () => {
+  const { data: session, status } = useSession();
+  
+  const isAuthenticated = status === "authenticated" && session?.user;
+  console.log('session', session);
+  console.log('status', status);
+  
   return (
     <header className="bg-background/95 supports-backdrop-filter:bg-background/60 sticky top-0 z-50 w-full border-b backdrop-blur">
       <div className="container flex h-16 items-center justify-between px-4">
@@ -21,7 +31,13 @@ const Navbar = () => {
           <ModeToggle />
           <ShopingCartButton />
           {/* TODO only show UserDropdown if we have a user, otherwise show sign in button */}
-          <UserDropdown />
+          {isAuthenticated ? (
+            <UserDropdown />
+          ) : (
+            <Button className="bg-primary text-primary-foreground hover:bg-primary/80">
+              <Link href={ROUTES.REGISTER}>Sign In</Link>
+            </Button>
+          )}
           {/* Mobile Menu */}
           <HamburgerMenu />
         </div>
