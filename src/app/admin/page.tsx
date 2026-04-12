b5f9d237f86281b37dfe6ceb0c5fe30a36e93ed9
BoyanK95 <boyan_k_95@abv.bg>
2026-04-13 00:07:21 +0300

    refacotr: comment whole useEffect and add LoadingText component to loading status

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,27 +1,27 @@
 "use client";
 
 import { useSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
-import { ROUTES } from "@/lib/constants/routes";
+// import { useRouter } from "next/navigation";
+// import { useEffect } from "react";
+// import { ROUTES } from "@/lib/constants/routes";
 import AdminForm from "@/components/Admin/AdminFormBoard/AdminForm";
 import AdminPosts from "@/components/Admin/AdminPosts/AdminPosts";
+import LoadingText from "@/components/LoadingText";
 
 export default function AdminPage() {
   const { data: session, status } = useSession();
-  const router = useRouter();
+  // const router = useRouter();
 
+  // TODO add this when we have roles implemented
+  // useEffect(() => {
+  //   if (status === "loading") return;
+  //   if (session?.user.role !== "ADMIN") {
+  //     router.push(ROUTES.HOME);
+  //   }
+  // }, [session, status, router]);
 
-  useEffect(() => {
-    if (status === "loading") return;
-    //TODO add this when we have roles implemented
-    // if (!session || session.user.role !== "ADMIN") {
-    //   router.push(ROUTES.HOME);
-    // }
-  }, [session, status, router]);
-
-  if (status === "loading") return <div>Loading...</div>;
-
+  if (status === "loading")
+    return <LoadingText text="Loading admin dashboard..." />;
 
   return (
     <div className="container mx-auto p-4">
