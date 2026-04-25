98b3fb59aed64be5abcfe9d92b9bcafc8700b1a1
BoyanK95 <boyan_k_95@abv.bg>
2026-04-25 17:28:18 +0300

    fix: config and create credentials

--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -1,9 +1,11 @@
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { type DefaultSession, type NextAuthOptions } from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-
+import { type DefaultSession, type NextAuthConfig } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import Facebook from "next-auth/providers/facebook";
+import Google from "next-auth/providers/google";
 import { db } from "@/server/db";
-import { Role } from "generated/prisma";
+import { ROUTES } from "@/lib/constants/routes";
+import bcrypt from "bcryptjs"; // Import the entire bcryptjs package
 
 /**
  * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
@@ -12,18 +14,13 @@ import { Role } from "generated/prisma";
  * @see https://next-auth.js.org/getting-started/typescript#module-augmentation
  */
 declare module "next-auth" {
-  interface User {
-    role: Role;
-  }
   interface Session extends DefaultSession {
     user: {
       id: string;
-      role: "USER" | "ADMIN";
       // ...other properties
       // role: UserRole;
     } & DefaultSession["user"];
   }
-
   // interface User {
   //   // ...other properties
   //   // role: UserRole;
@@ -36,34 +33,85 @@ declare module "next-auth" {
  * @see https://next-auth.js.org/configuration/options
  */
 export const authConfig = {
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    }),
-  ],
   adapter: PrismaAdapter(db),
-  callbacks: {
-    async signIn({ user }) {
-      const dbUser = await db.user.findUnique({ where: { id: user.id } });
-      if (!dbUser) return true;
+  session: {
+    strategy: "jwt",
+  },
+  pages: {
+    signIn: ROUTES.LOGIN,
+    newUser: ROUTES.REGISTER,
+  },
+  providers: [
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error("Enter email or password!");
+        }
 
-      const userCount = await db.user.count();
-      if (userCount === 1) {
-        await db.user.update({
-          where: { id: user.id },
-          data: { role: Role.ADMIN },
+        const user = await db.user.findUnique({
+          where: { email: credentials.email as string },
         });
-      }
-      return true;
-    },
-    session: ({ session, user }) => ({
-      ...session,
-      user: {
-        ...session.user,
-        id: user.id,
-        role: user.role,
+
+        if (!user?.password) {
+          throw new Error("No password!");
+        }
+        try {
+          const isPasswordValid = await bcrypt.compare(
+            credentials.password as string,
+            user.password,
+          );
+          if (!isPasswordValid) {
+            throw new Error('Password is invalid!');
+          }
+        } catch (error) {
+          throw new Error(error as string)
+        }
+
+        return {
+          id: user.id,
+          email: user.email,
+          name: user.name,
+          image: user.image,
+        };
       },
     }),
+    Facebook({
+      clientId: process.env.FACEBOOK_CLIENT_ID,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+      allowDangerousEmailAccountLinking: true, // Add this line
+    }),
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      allowDangerousEmailAccountLinking: true, // Add this line
+    }),
+    /**
+     * ...add more providers here.
+     *
+     * Most other providers require a bit more work than the Discord provider. For example, the
+     * GitHub provider requires you to add the `refresh_token_expires_in` field to the Account
+     * model. Refer to the NextAuth.js docs for the provider you want to use. Example:
+     *
+     * @see https://next-auth.js.org/providers/github
+     */
+  ],
+  callbacks: {
+    async session({ session, token }) {
+      if (token && session.user) {
+        session.user.id = token.sub!;
+      }
+      return session;
+    },
+    async jwt({ token, user }) {
+      if (user) {
+        token.sub = user.id;
+      }
+      return token;
+    },
   },
-} satisfies NextAuthOptions;
+} satisfies NextAuthConfig;
